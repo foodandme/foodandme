3b4aa659e6a91be44f1e7680222b8cccdb991429
David Jeanmonod <david.jeanmonod@gmail.com>
2017-02-10 18:45:11 +0100

    Introduce Bootstrap

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $.getJSON("data/allergens.json", function( data ) {
     var items = [];
-    $.each( data, function( key, val ) {
-        console.log(key, val);
-        items.push( "<input type='checkbox' id='" + key + "'>" + key + "</input>" );
+    $.each( data, function( key, list ) {
+        localStorage.setItem("allergens-product-list", list);
+        items.push('' +
+            '<div class="checkbox"><label> ' +
+                '<input type="checkbox" value="'+key+'">' + key + '</input>' +
+            '</label></div>'
+        );
     });
-
     $(items.join( "")).appendTo('#restrictions');
 });
